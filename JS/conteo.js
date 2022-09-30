17b47972161736fd7243b760cc3828f02705b1f8
Danielitorc <drangel664@gmail.com>
2022-09-30 00:34:22 -0500

    Quedo hasta cuenta regresiva

---
+++ JS/conteo.js
@@ -0,0 +1,28 @@
+const $days = document.getElementById('days');
+const $hours = document.getElementById('hours');
+const $minutes = document.getElementById('minutes');
+const $seconds = document.getElementById('seconds');
+
+//Fecha a futuro
+const countDownDate = new Date('Oct 17 2022 17:00:00').getTime();
+
+
+let interval = setInterval(function(){
+    //Obtener fecha actual
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre fechas
+    let distance = countDownDate - now;
+
+    //Calculo a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60 * 60)) / (1000));
+    
+    //Muestro resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+}, 1000);
